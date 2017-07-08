a7491631abd70fa2bd370c7df2570d766ca9e53c
Dhruvesh Shah <dhruveshshah@users.noreply.github.com>
2017-07-07 23:40:42 -0500

    R script

--- Vietnam War.R
+++ Vietnam War.R
@@ -17,3 +17,12 @@ data <- read.csv('..Kaggle\\Vietnam_War\\VietnamConflict.csv', na.strings=c("","
 # load Vietnam Map
 gadm <- readRDS("..Kaggle\\Vietnam_War\\Vietnam_GADM_MAP_1.rds")
 head(data)
+
+#resize plot output
+options(repr.plot.width=5, repr.plot.height=5)
+# missing data plot
+missmap(data)
+# change dates into date format
+data$BIRTH_YEAR <- as.Date(as.character(data$BIRTH_YEAR), "%Y%m%d")
+data$FATALITY_DATE <- as.Date(as.character(data$FATALITY_DATE), "%Y%m%d")
+
