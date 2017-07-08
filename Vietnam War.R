e0e7d2026550b03249feee25dc985e5aa1c43f1e
Dhruvesh Shah <dhruveshshah@users.noreply.github.com>
2017-07-07 23:34:52 -0500

    R script

--- Vietnam War.R
+++ Vietnam War.R
@@ -1,13 +1,19 @@
-#Installing required libraries
+install.packages("ggplot2")
 library(ggplot2)
+install.packages("plyr")
 library(plyr)
+install.packages("RColorBrewer")
 library(RColorBrewer)
+install.packages("maps")
 library(maps)
+install.packages("sp")
 library(sp)
 library(Amelia)
 
+setwd("..\\Kaggle\\Vietnam_War")
+
 #Load data
-data <- read.csv('/kaggle/input/VietnamConflict.csv', na.strings=c("","NA")) 
+data <- read.csv('..Kaggle\\Vietnam_War\\VietnamConflict.csv', na.strings=c("","NA")) 
 # load Vietnam Map
-gadm <- readRDS("/kaggle/input/Vietnam_GADM_MAP_1.rds")
+gadm <- readRDS("..Kaggle\\Vietnam_War\\Vietnam_GADM_MAP_1.rds")
 head(data)
